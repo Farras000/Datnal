bae185704807e22f94be34950e8055fe8c27c628
Ayas <149809452+Farras000@users.noreply.github.com>
2025-12-11 11:59:42 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+
+
+model = load_model("model_1.keras")
+
+class_names = ["Glioma", "Meningioma", "Pituitary"]
+
+def preprocess(img):
+    img = img.resize((150, 150))  
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+# UI
+st.title("Brain Tumor Classification App")
+st.write("Upload gambar MRI otak untuk mendeteksi jenis tumor.")
+
+uploaded_file = st.file_uploader("Upload Brain MRI", type=["jpg", "jpeg", "png"])
+
+if uploaded_file:
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded Image")
+
+    if st.button("Predict"):
+        input_tensor = preprocess(img)
+        pred = model.predict(input_tensor)
+        class_id = np.argmax(pred)
+        confidence = float(np.max(pred))
+
+        st.success(f"Prediction: **{class_names[class_id]}**")
+        st.info(f"Confidence: {confidence:.4f}")
